20b4cb08d77a102e6b892671f00428772471116f
ramiro <ramiro@tinybird.co>
2024-01-09 15:39:33 +0100

    fix script, add persist docs

--- set_env_var.sh
+++ set_env_var.sh
@@ -1,4 +1,4 @@
 export DBT_PROFILES_DIR=./
 export DBT_PROJECT_DIR=./
 export DBT_PROFILE=ramiro-dev
-export DBT_GOOGLE_KEYFILE=/home/ramiro/tb/dbt_project/.gcp_credentials/ramiro-sa.json
+export GCP_SERVICE_ACCOUNT_KEYFILE=/home/ramiro/tb/dbt_project/.gcp_credentials/ramiro-sa.json
